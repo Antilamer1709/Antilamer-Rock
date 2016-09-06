74ae9c767c0b340299ee64e81493ef03ff1331e1
antilamer <antilamer1709@gmail.com>
2016-09-06 16:59:15 +0200

    Cleaning directories

--- src/main/java/com/antilamer/controller/IndexController.java
+++ src/main/java/com/antilamer/controller/IndexController.java
@@ -2,23 +2,16 @@ package com.antilamer.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.stereotype.Controller;
 
 /**
  * Created by antilamer on 03.09.16.
  */
-//@RestController
 @Controller
 public class IndexController {
-    //    @RequestMapping("/")
-//    @RequestMapping(value = "/")
-//    public String index() {
-//        return "index";
-//    }
 
     @RequestMapping(value="/",method = RequestMethod.GET)
     public String homepage(){
-        return "index";
+        return "index.html";
     }
 }
