d4c0273352ad097ac2c46e4a2a54d3d5b010ccb9
antilamer <antilamer1709@gmail.com>
2016-11-22 20:44:13 +0100

    Predeploying settings

---
+++ src/main/java/com/antilamer/config/WebInitializer.java
@@ -0,0 +1,12 @@
+package com.antilamer.config;
+
+import com.antilamer.SpringWebAppApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class WebInitializer extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringWebAppApplication.class);
+    }
+}
