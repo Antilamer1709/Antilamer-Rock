31a73599b787f3996503753bea4ec29e79a7d245
antilamer <antilamer1709@gmail.com>
2016-11-25 13:38:19 +0100

    Login and registration blocking

--- src/main/webapp/JS/controllers/index.js
+++ src/main/webapp/JS/controllers/index.js
@@ -1,5 +1,6 @@
 angular.module('mvcApp').controller('IndexCtrl', ['$scope', '$rootScope', 'UserService', '$route', '$location', '$window', 'ROLES', 'ngToast', 'CommonService', function ($scope, $rootScope, UserService, $route, $location, $window, ROLES, ngToast, CommonService) {
 
+    $rootScope.block = false;
     $rootScope.home = true;
     $rootScope.bands = false;
     $scope.active = "active";
@@ -13,23 +14,34 @@ angular.module('mvcApp').controller('IndexCtrl', ['$scope', '$rootScope', 'UserS
     ngToast.settings.horizontalPosition = 'center';
 
     $scope.submitRegistration = function () {
-        UserService.registration($scope.registration, function (res) {
-            CommonService.showToast('success', 'Registration success');
-            $scope.login.username = $scope.registration.username;
-            $scope.login.password = $scope.registration.password;
-            $scope.submitLogin();
-        }, function (err) {
-            $rootScope.error = err;
-        })
+        if (!$rootScope.block) {
+            $rootScope.block = true;
+            UserService.registration($scope.registration, function (res) {
+                CommonService.showToast('success', 'Registration success');
+                $scope.login.username = $scope.registration.username;
+                $scope.login.password = $scope.registration.password;
+                $rootScope.block = false;
+                $scope.submitLogin();
+            }, function (err) {
+                $rootScope.block = false;
+                $rootScope.error = err;
+            })
+        }
     };
     $scope.submitLogin = function () {
-        UserService.login($scope.login, function (res) {
-            CommonService.showToast('success', 'You are logged');
-            $scope.getLoggerUser();
-        }, function (err) {
-            $rootScope.error = err;
-        })
+        if (!$rootScope.block) {
+            $rootScope.block = true;
+            UserService.login($scope.login, function (res) {
+                CommonService.showToast('success', 'You are logged');
+                $scope.getLoggerUser();
+                $rootScope.block = false;
+            }, function (err) {
+                $rootScope.block = false;
+                $rootScope.error = err;
+            })
+        }
     };
+
 
     $scope.getLoggerUser = function () {
         UserService.getLoggedUser(function (res) {
